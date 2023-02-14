5b2cb90c06e026ae832c2a575ab93a8d45968516
Srivastava <abhishek.g.srivastava@capgemini.com>
2023-02-14 09:04:42 +0530

    component create for site dashboard

--- bpa-demo/src/app/app-routing.module.ts
+++ bpa-demo/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SiteDashboardComponent } from './site-dashboard/site-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SiteDashboardComponent },
+  { path: 'site', component: SiteDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bpa-demo/src/app/app.module.ts
+++ bpa-demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteDashboardComponent } from './site-dashboard/site-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteDashboardComponent
   ],
   imports: [
     BrowserModule,
